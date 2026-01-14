cd690e29f95c19dcc5f65ac1df38bb7906d0c12b
Ori <150680210+OrianaFawkes@users.noreply.github.com>
2026-01-15 02:25:15 +0800

    Refactor CSS
    - move font-face and typography rules from styles.css to a new typography.css file and color variables to a new theme.css file
    - update styles.css to use CSS variables and modernize layout and component styles
    - Minor template fix in highlightMatch function in render.js.

--- js/render.js
+++ js/render.js
@@ -36,8 +36,9 @@ export function highlightMatch(text, query) {
   if (index === -1) return text;
   return `
     ${text.slice(0, index)}
-    <strong>${text.slice(index, index + query.length)}</strong>
-    ${text.slice(index + query.length)}
+    <strong>${text.slice(index, index + query.length)}</strong>${text.slice(
+    index + query.length
+  )}
   `;
 }
 
